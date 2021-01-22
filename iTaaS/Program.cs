629fd152f1a0e470a0ea1304947da539cafe64c6
wy7-source <wythorbazan@gmail.com>
2021-01-22 18:23:33 -0300

    iTaaS Finished, lets do tests

--- iTaaS/Program.cs
+++ iTaaS/Program.cs
@@ -7,9 +7,25 @@ namespace CandidateTesting.WythorFerreiraBazan.iTaaS
     {
         static void Main(string[] args)
         {
-            Scrape scrape = new Scrape();
-            Console.WriteLine(scrape.WebScrape(args[0]));
-
+            try
+            {
+                Uri url = new Uri(args[0]);
+                Scrape scrape = new Scrape();
+                var log = scrape.GetLogFile(url);
+                scrape.ConvertFile(log,args[1]);
+            }
+            catch(IndexOutOfRangeException)
+            {
+                throw new Exception("Missing one or more arguments on command line!");
+            }
+            catch(UriFormatException)
+            {
+                throw new Exception("Invalid URI to get MINHA CDN");
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Something went Wrong:\n "+e);
+            }
         }
     }
 }
--- iTaaS/Scrape.cs
+++ iTaaS/Scrape.cs
@@ -2,13 +2,14 @@ using System;
 using System.Text;
 using System.Net;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace CandidateTesting.WythorFerreiraBazan.iTaaS
 {
     class Scrape
     {
-        // WebScrape is our method that download the MINHA CDN file.
-        public MatchCollection WebScrape(string uri)
+        // GetLogFile is our method that download the MINHA CDN file.
+        public MatchCollection GetLogFile(Uri uri)
         {
             MatchCollection encontrados;
             try
@@ -37,6 +38,21 @@ namespace CandidateTesting.WythorFerreiraBazan.iTaaS
                 throw e;
             }
             return encontrados;
+        }
+        public void ConvertFile(MatchCollection collection, string localToSaveFile)
+        {
+            List<string> lines = new List<string>();
+            lines.Add("#Version: 1.0\n #Date: "+ DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss") + "\n #Fields: provider http-method status-code uri-path response-size cache-status");
+            foreach(Match item in collection)
+            {
+                string cacheCode = item.Groups[3].Value;
+                string cacheCodeChanged = cacheCode == "INVALIDATE" ? "REFRESH_HIT" : item.Groups[3].Value;
+                int timeTakenChanged = Convert.ToInt32(Math.Round(Convert.ToDouble(item.Groups[6].Value),5));
+                
+                lines.Add("\"MINHA CDN\" "+ item.Groups[4] + " " + item.Groups[2]+ " " + item.Groups[5]+ " " + timeTakenChanged+ " " + item.Groups[1]+ " " + cacheCodeChanged);
+            }
+
+            System.IO.File.WriteAllLines(localToSaveFile, lines.ToArray());
         }
     }
 }
